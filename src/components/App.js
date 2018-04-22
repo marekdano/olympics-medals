31bd56e87e9fd07b90a3f3339e52662507212d3a
Marek Dano <mk.dano@gmail.com>
2018-04-22 23:03:31 +0100

    Display the list of athletes with their medals.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
+import medalists from "../data/olympics_medalists";
 
 class App extends Component {
+  state = {
+    medalists
+  };
+
   render() {
-    return <div>Welcome in app</div>;
+    return (
+      <div>
+        <h1>Athletes</h1>
+        {this.state.medalists.map(medalist => {
+          return (
+            <div key={medalist.athlete + medalist.event}>
+              <h3>Athlete</h3>
+              <p>{medalist.athlete}</p>
+              <p>medal: {medalist.medal}</p>
+            </div>
+          );
+        })}
+        <p>{this.state.medalists[0].athlete}</p>
+      </div>
+    );
   }
 }
 
