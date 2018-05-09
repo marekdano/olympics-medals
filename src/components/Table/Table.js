3b816b3a955d72e93cb24cd373499d0343320652
Marek Dano <mk.dano@gmail.com>
2018-05-09 19:47:54 +0100

    Add layout to the Table component.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-const table = props => (
-  <div>
-    <h3>Table displayed here</h3>
-  </div>
+const table = ({ rows }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Country</th>
+        <th>Gold</th>
+        <th>Silver</th>
+        <th>Bronze</th>
+        <th>Total</th>
+      </tr>
+    </thead>
+    <tbody>
+      {rows &&
+        rows.map(row => {
+          return (
+            <tr key={row.country}>
+              <td>{row.country}</td>
+              <td>{row.totalGold}</td>
+              <td>{row.totalSilver}</td>
+              <td>{row.totalBronze}</td>
+              <td>{row.athletes.length}</td>
+            </tr>
+          );
+        })}
+    </tbody>
+  </table>
 );
+
+// TODO: add PropsType
 
 export default table;
